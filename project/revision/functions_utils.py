a68768dc764c3fb069ff2a94504ec622bdc1cdda
Pavlo Yermolenko <pavloyermolenko8@gmail.com>
2025-09-27 20:19:08 +0200

    tickets

--- project/revision/functions_utils.py
+++ project/revision/functions_utils.py
@@ -6,6 +6,17 @@ def add_salt_to_list(given_list: list) -> None:
     """WARNING! list is being modified globally"""
 
 
+def get_ticket_price(age:int) -> float:
+    base_price = 100
 
-
+    if age < 6:
+        base_price = 0
+        return base_price
+    elif 6 <= age <= 17:
+        base_price = base_price / 2
+        return base_price
+    elif 18 <= age <= 59:
+        return base_price
+    else:
+        return base_price * 0.7
 
