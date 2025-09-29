ee2efe721aa27b3d01b37b6c27c1c235726dc91e
Pavlo Yermolenko <pavloyermolenko8@gmail.com>
2025-09-29 22:08:00 +0200

    123123

--- project/hw12/decorator_with_options.py
+++ project/hw12/decorator_with_options.py
@@ -12,5 +12,9 @@ def round_result(ndigits: int):
 def divide_with_rounding(a, b):
     return a / b
 
+@round_result(3)
+def addition_with_rounding(c, d):
+    return c + d
 
 print(divide_with_rounding(1, 3))
+print(addition_with_rounding(2, 3.333333))
